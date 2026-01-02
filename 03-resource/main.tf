581348fec6a5c2980a82642d9ff341831ddec2ab
Ravina <porwal.ravina@gmail.com>
2026-01-02 20:01:08 +0400

    first shell script

--- 03-resource/main.tf
+++ 03-resource/main.tf
@@ -15,7 +15,7 @@ resource "azurerm_virtual_machine" "main" {
   location              = data.azurerm_resource_group.example.location
   resource_group_name   = data.azurerm_resource_group.example.name
   network_interface_ids = [azurerm_network_interface.example.id]
-  vm_size               = "Standard_DS1_v2"
+  vm_size               = "Standard_B2s"
 
   # Uncomment this line to delete the OS disk automatically when deleting the VM
   delete_os_disk_on_termination = true

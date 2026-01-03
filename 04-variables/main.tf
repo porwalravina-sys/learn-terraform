bdfe901424982c1473877d8b737ddf85d711949b
Ravina <porwal.ravina@gmail.com>
2026-01-03 18:44:46 +0400

    first shell script

---
+++ 04-variables/main.tf
@@ -0,0 +1,39 @@
+variable "name" {
+    default = "Hello"
+    }
+
+variable "n1" {
+    default = 2
+    }
+
+variable "b1" {
+    default = true
+    }
+
+variable "v1" {
+    default "Hello"
+    }
+
+variable "v2" {
+    default = [
+        "Hello",
+        2,
+        false
+        ]
+    }
+
+variable "v3" {
+     default = {
+         course = "devops"
+         cloud = "azure"
+         }
+    }
+
+output "v1" {
+    value = var.v1
+    }
+
+output "v2" {
+    value = "${var.v1} - John"
+    }
+

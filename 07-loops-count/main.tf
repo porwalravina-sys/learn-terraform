c21e9270f49d15dee736a10e5a2745ab75ffe329
Ravina <porwal.ravina@gmail.com>
2026-01-04 23:51:55 +0400

    first shell script

--- 07-loops-count/main.tf
+++ 07-loops-count/main.tf
@@ -5,6 +5,9 @@
 
 resource "null_resource" "demo" {
     count = length(var.demo)
+    provisioner "local-exec" {
+        command = "echo ${var.demo[count.index]}"
+        }
     }
 
 variable "demo" {

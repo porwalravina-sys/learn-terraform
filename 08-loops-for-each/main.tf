6867d20da519f734e31e66fc08036cf2cdfd5a0c
Ravina <porwal.ravina@gmail.com>
2026-01-05 01:17:02 +0400

    first shell script

--- 08-loops-for-each/main.tf
+++ 08-loops-for-each/main.tf
@@ -1,12 +1,19 @@
 resource "local_file" "foo" {
     for_each = var.demo
-    content = each.value
+    content = each.value["content"]
     filename = "/tmp/${each.key}"
     }
 
 variable "demo" {
     default = {
-        ORANGE = "orange"
-        BANANA = "banana"
+        APPLE = {
+            content = "apple is good for health"
+            }
+        ORANGE = {
+            content = "good for vitamin c"
+            }
+        BANANA = {
+            content = "good for vitamin"
+            }
         }
     }
